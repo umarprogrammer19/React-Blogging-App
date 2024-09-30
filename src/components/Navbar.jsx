24dfcb96ceec3a7f04e98c72215daf6f31149c5e
umarprogrammer19 <umarprogrammer19666@gmail.com>
2024-10-01 02:48:49 +0500

    Navbar Completed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,7 +15,7 @@ export default function Navbar() {
                 setUser(null);
             }
         });
-
+        
         return () => unsubscribe();
     }, []);
 
@@ -26,11 +26,16 @@ export default function Navbar() {
     };
 
     return (
-        <div className="navbar bg-primary">
+        <div className="navbar bg-primary px-6">
             <div className="flex-1">
                 <a className="btn btn-ghost text-xl text-white">Personal Blogging App</a>
             </div>
-            <div className="flex-none">
+            <div className="flex-none flex items-center space-x-2">
+                {user && (
+                    <span className="text-white text-lg font-semibold cursor-pointer`">
+                        {user.firstName} {user.lastName}
+                    </span>
+                )}
                 <div className="dropdown dropdown-end">
                     <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
                         <div className="w-10 rounded-full">
@@ -52,14 +57,14 @@ export default function Navbar() {
                         className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
                         {user ? (
                             <>
-                                <li><Link to={"/dashboard"}>Dashboard</Link></li>
-                                <li><Link to={"/"}>Blogs</Link></li>
-                                <li><button onClick={handleLogout}>Logout</button></li>
+                                <li><Link className="hover:bg-black hover:text-white" to={"/dashboard"}>Dashboard</Link></li>
+                                <li><Link className="hover:bg-black hover:text-white" to={"/"}>Blogs</Link></li>
+                                <li><button className="hover:bg-black hover:text-white" onClick={handleLogout}>Logout</button></li>
                             </>
                         ) : (
                             <>
-                                <li><Link to={"/login"}>Login</Link></li>
-                                <li><Link to={"/signup"}>Sign Up</Link></li>
+                                <li><Link className="hover:bg-black hover:text-white" to={"/login"}>Login</Link></li>
+                                <li><Link className="hover:bg-black hover:text-white" to={"/signup"}>Sign Up</Link></li>
                             </>
                         )}
                     </ul>
