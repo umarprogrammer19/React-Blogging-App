a444c22bafb12e845720328703d5544ee8fab786
umarprogrammer19 <umarprogrammer19666@gmail.com>
2024-10-01 00:07:34 +0500

    Routing And Tailwind Configured

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,27 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { createRoot } from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './index.css';
+import Layout from './Layout.jsx';
+import SignUp from './pages/signUp.jsx';
+import Login from './pages/login.jsx';
+import Blogs from './pages/blogs.jsx';
+
+const router = createBrowserRouter([{
+  path: "/",
+  element: <Layout />,
+  children: [{
+    path: "",
+    element: <Blogs />
+  }, {
+    path: "/signup",
+    element: <SignUp />
+  }, {
+    path: "/login",
+    element: <Login />
+  }]
+}])
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <RouterProvider router={router}>
+  </RouterProvider>
 )
