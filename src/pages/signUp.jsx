bf4383114d64e37723920dc9f7df2feea6fb91b8
umarprogrammer19 <umarprogrammer19666@gmail.com>
2024-10-04 02:20:37 +0500

    Added Loading State

--- src/pages/signUp.jsx
+++ src/pages/signUp.jsx
@@ -172,7 +172,26 @@ const SignUp = () => {
                             disabled={loading}
                             className={`w-full bg-purple-600 text-white py-3 rounded-lg hover:bg-purple-700 transition ${loading ? 'opacity-50' : ''}`}
                         >
-                            {loading ? 'Signing Up...' : 'Sign Up'}
+                            {loading ?
+                                <div className="flex justify-center items-center space-x-2">
+                                    <svg className="w-5 h-5 animate-spin text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                                        <circle
+                                            className="opacity-25"
+                                            cx="12"
+                                            cy="12"
+                                            r="10"
+                                            stroke="currentColor"
+                                            strokeWidth="4"
+                                        ></circle>
+                                        <path
+                                            className="opacity-75"
+                                            fill="currentColor"
+                                            d="M4 12a8 8 0 018-8v8H4z"
+                                        ></path>
+                                    </svg>
+                                    <span>Signing Up ...</span>
+                                </div>
+                                : 'Sign Up'}
                         </button>
                     </form>
 
